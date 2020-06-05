eb91b2195f4c713997a46d22576598bd2580dee8
Ionut Radu <ionut.radu@tremend.com>
2020-06-05 14:03:17 +0300

    modify default value

--- scripts/set_solrhost.sh
+++ scripts/set_solrhost.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export SOLR_HOST=$HOST
-JUTE_MAXBUFFER=${JUTE_MAXBUFFER:-0x20000000}
+JUTE_MAXBUFFER=${JUTE_MAXBUFFER:-50000000}
 export SOLR_OPTS="${SOLR_OPTS} -Djute.maxbuffer=${JUTE_MAXBUFFER}"
 
 cp -ra /opt/solr/server/solr_orig/* /opt/solr/server/solr/.
