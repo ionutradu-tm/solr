66eba7073da1539835fb9d2217436e1a51afe975
Ionut Radu <ionut.radu@tremend.com>
2020-06-05 13:13:42 +0300

    add JUTE_MAXBUFFER option

--- scripts/set_solrhost.sh
+++ scripts/set_solrhost.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export SOLR_HOST=$HOST
+JUTE_MAXBUFFER=${JUTE_MAXBUFFER:-0x20000000}
+export SOLR_OPTS="${SOLR_OPTS} -Djute.maxbuffer=${JUTE_MAXBUFFER}"
 
 cp -ra /opt/solr/server/solr_orig/* /opt/solr/server/solr/.
 
